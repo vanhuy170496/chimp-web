f57d1c3a3cd79f180a6667b1a1b98bb1b425bedb
huynv4 <huynv4@vng.com.vn>
2022-11-29 18:51:50 +0700

    update qa

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,15 +1,53 @@
 import {getStarknet, connect, IStarknetWindowObject} from "get-starknet-wallet";
-import {StarknetChainId} from "starknet/dist/constants";
-import {compileCalldata} from "starknet/dist/utils/stark";
+import {uint256} from "starknet";
 
-const NFT_CONTRACT_ADDRESS = "0x02d65f093bb27010affdab3a051951b11119bdf9b45ba6927c5e7ddec27c130c";
+const NFT_CONTRACT_ADDRESS = "0x028cfe0c6b27abcad8957262cdf0ea92ca1261f10682968af1a82a8b5fb73a3d";
+const EHT_CONTRACT_ADDRESS = "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7";
 
-const connectWallet = async (): Promise<IStarknetWindowObject> => {
-	const starknet = await connect({showList: true});
-	if (!starknet) return;
-	await starknet.enable({showModal: true});
+const CONNECTED_KEY = "CONNECTED_KEY";
+
+const connectWallet = async (showList = true, showModal = true): Promise<IStarknetWindowObject> => {
+	let starknet = getStarknet();
+	if (!starknet?.isConnected) {
+		starknet = await connect({showList});
+		if (!starknet) return;
+		await starknet.enable({showModal});
+	}
+
+	if (starknet.isConnected) {
+		localStorage.setItem(CONNECTED_KEY, "1");
+		const address = starknet.account.address;
+		const shortAddress = address.substring(0, 4) + "..." + address.substring(address.length - 4);
+		document.getElementById("connect-value").innerHTML = shortAddress;
+		document.getElementById("connect").classList.add("connected");
+	} else {
+		localStorage.setItem(CONNECTED_KEY, "0");
+		document.getElementById("connect").classList.remove("connected");
+		document.getElementById("connect-value").innerHTML = "CONNECT";
+	}
+
 	return starknet;
 };
+
+try {
+	if (localStorage.getItem(CONNECTED_KEY) === "1") {
+		connectWallet(false, false);
+	}
+} catch (error) {}
+
+document.getElementById("connect").addEventListener("click", async (event) => {
+	event.preventDefault();
+	let starknet = getStarknet();
+	if (!starknet.isConnected) {
+		starknet = await connectWallet();
+	} else {
+		localStorage.setItem(CONNECTED_KEY, "0");
+		document.getElementById("connect").classList.remove("connected");
+		document.getElementById("connect-value").innerHTML = "CONNECT";
+		starknet.isConnected = false;
+	}
+	return false;
+});
 
 document.getElementById("buy-now").addEventListener("click", async (event) => {
 	event.preventDefault();
@@ -19,32 +57,34 @@ document.getElementById("buy-now").addEventListener("click", async (event) => {
 	return false;
 });
 
-document.getElementById("buy-popup-og-submit").addEventListener("click", async (event) => {
-	event.preventDefault();
-	const starknet = getStarknet();
-	if (!starknet.isConnected) return false;
-	//@ts-ignore
-	const amount = parseInt(document.getElementById("buy-popup-og-amount").value);
-	const res = await starknet.account.execute([
-		{
-			contractAddress: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-			entrypoint: "approve",
-			calldata: compileCalldata({
-				recipient: "0x007d6b73c3e83a368b44d432f0e6c732798e1191fbd04546fd5cff748ae8375a",
-				amount: "10000000000000000",
+const mint = (type: string, priceUnit: number, entrypoint: string) => {
+	document.getElementById(`buy-popup-${type}-submit`).addEventListener("click", async (event) => {
+		event.preventDefault();
+		const starknet = await connectWallet();
+		if (!starknet) return false;
+
+		//@ts-ignore
+		const amount = parseInt(document.getElementById(`buy-popup-${type}-amount`).value);
+		const approvePrice = uint256.bnToUint256(amount * priceUnit + "0000000000000000").low;
+
+		await starknet.account.execute([
+			{
+				contractAddress: EHT_CONTRACT_ADDRESS,
+				entrypoint: "approve",
+				calldata: [NFT_CONTRACT_ADDRESS, approvePrice, 0],
+			},
+			...Array(amount).fill({
+				contractAddress: NFT_CONTRACT_ADDRESS,
+				entrypoint: entrypoint,
+				calldata: [starknet.account.address],
 			}),
-		},
-		{
-			contractAddress: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
-			entrypoint: "transferFrom",
-			calldata: compileCalldata({
-				sender: starknet.account.address,
-				recipient: "0x007d6b73c3e83a368b44d432f0e6c732798e1191fbd04546fd5cff748ae8375a",
-				amount: "10000000000000000",
-			}),
-		},
-	]);
-	console.log(res);
-	alert("Transaction sending....");
-	return false;
-});
+		]);
+
+		alert("Transaction sending....");
+		return false;
+	});
+};
+
+mint("og", 1, "mintOG");
+mint("whitelist", 2, "mintWhitelist");
+mint("publish", 2, "mintPublish");
